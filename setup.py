2c52daed43dc7ad54c58df0b159f4fab684b12b2
Jurgen Blignaut <jurgen@webtide.co.za>
2013-06-19 11:42:14 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mama-survey',
+    version='dev',
+    description='Simple Survey Django Application for askMAMA.mobi',
+    long_description = open('README.rst', 'r').read() + open('AUTHORS.rst', 'r').read() + open('CHANGELOG.rst', 'r').read(),
+    author='Praekelt Foundation',
+    author_email='dev@praekeltfoundation.com',
+    license='BSD',
+    # url='http://github.com/praekelt/mama-survey',
+    packages = find_packages(),
+    dependency_links = [
+    ],
+    install_requires = [
+        'django>=1.4,<1.5',
+    ],
+    tests_require=[
+        'django-setuptest',
+    ],
+    # test_suite="setuptest.setuptest.SetupTestSuite",
+    include_package_data=True,
+    classifiers = [
+        "Programming Language :: Python",
+        "License :: OSI Approved :: BSD License",
+        "Development Status :: 4 - Beta",
+        "Operating System :: OS Independent",
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+    ],
+    zip_safe=False,
+)
