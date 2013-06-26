b1454eb842de310a625f83d55f8b0e024732a20d
Jurgen Blignaut <jurgen@webtide.co.za>
2013-06-26 13:43:28 +0200

    Implemented template tag to show available survey on home page

---
+++ survey/templatetags/survey_inclusion_tags.py
@@ -0,0 +1,21 @@
+from copy import copy
+
+from django import template
+
+from survey.models import Questionnaire
+
+
+register = template.Library()
+
+@register.inclusion_tag('survey/inclusion_tags/survey_listing.html',
+                        takes_context=True)
+def show_survey(context):
+    context = copy(context)
+    user = context['user']
+    if user.is_authenticated():
+        survey = Questionnaire.objects.questionnaire_for_user(user)
+        context.update({
+            'survey': survey
+        })
+    return context
+
