545600f42eb744623a067f6fa71a41a2ecc2f3e5
Jurgen Blignaut <jurgen@webtide.co.za>
2013-07-02 16:27:47 +0200

    Implemented as_div method in forms module and removed jmbo dependency

--- survey/forms.py
+++ survey/forms.py
@@ -3,7 +3,21 @@ from django.forms.widgets import RadioSelect
 
 # from pml import forms as pml_forms
 from django import forms
-from jmbo.forms import as_div
+
+def as_div(form):
+    """This formatter arranges label, widget, help text and error messages by
+    using divs.  Apply to custom form classes, or use to monkey patch form
+    classes not under our direct control."""
+    # Yes, evil but the easiest way to set this property for all forms.
+    form.required_css_class = 'required'
+
+    return form._html_output(
+        normal_row=u'<div class="field"><div %(html_class_attr)s>%(label)s %(errors)s <div class="helptext">%(help_text)s</div> %(field)s</div></div>',
+        error_row=u'%s',
+        row_ender='</div>',
+        help_text_html=u'%s',
+        errors_on_separate_row=False
+    )
 
 
 _SURVEY_CHOICES = (
