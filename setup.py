d2667ea4f2423ebc6fba70ae0ea7963460595813
Simon de Haan <simon@praekeltfoundation.org>
2013-07-15 16:52:34 +0200

    bloody tabs

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
     ],
     tests_require=[
         'django-setuptest==0.1.4',
-	'mock',
+        'mock',
     ],
     test_suite="setuptest.setuptest.SetupTestSuite",
     include_package_data=True,
